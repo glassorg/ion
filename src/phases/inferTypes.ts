3851d65f0d60fce304ffc09d8307fa35d1201dba
Kris Nye <krisnye@gmail.com>
2020-05-07 23:49:52 -0700

    type inference and simplification first working

--- src/phases/inferTypes.ts
+++ src/phases/inferTypes.ts
@@ -4,75 +4,67 @@ import Analysis from "../ast/Analysis";
 import Expression from "../ast/Expression";
 import Literal from "../ast/Literal";
 import LiteralType from "../ast/LiteralType";
-import * as ast from "../ast"
-import createScopeMaps, { ScopeMap } from "../createScopeMaps";
-
-function getReturnStatements(node: ast.FunctionExpression): ast.ReturnStatement[] {
-    let statements: ast.ReturnStatement[] = []
-    traverse(node, {
-        enter(node) {
-            if (ast.CallExpression.is(node)) {
-                return skip
-            }
-        },
-        leave(node) {
-            if (ast.ReturnStatement.is(node)) {
-                statements.push(node)
-            }
-        }
-    })
-    return statements
-}
+import * as ast from "../ast";
+import createScopeMaps, { ScopeMap, ScopeMaps } from "../createScopeMaps";
+import getSortedTypedNodes from "./getSortedTypedNodes";
+import simplify from "./simplify";
 
 // that is some typescript kung fu right there.
-const getPredecessors: { [P in keyof typeof ast]?: (e: InstanceType<typeof ast[P]>, scope: ScopeMap) => Iterator<ast.Typed>} = {
-    *BinaryExpression(node) {
-        yield node.left
-        yield node.right
+export const inferTypeFunctions: { [P in keyof typeof ast]?: (e: InstanceType<typeof ast[P]>, resolved: { get<T>(t: T): T }, scope: ScopeMaps) => any} = {
+    BinaryExpression(node) {
     },
-    *Literal(node) {
+    UnionType(node) {
     },
-    *FunctionExpression(node) {
-        for (let param of node.parameters) {
-            yield param
-        }
-        for (let returnStatement of getReturnStatements(node)) {
-            yield returnStatement.value   
-        }
+    IntersectionType(node) {
     },
-    *Reference(node, scope) {
-        yield scope[node.name]
+    Literal(node) {
+        return node.patch({ type: new LiteralType({ location: node.location, literal: node }) })
     },
-    *MemberExpression(node) {
-        yield node.object
-        if (Expression.is(node.property)) {
-            yield node.property
-        }
+    LiteralType(node) {
     },
-    *ArrayExpression(node) {
-        for (let element of node.elements) {
-            yield element
-        }
+    ClassDeclaration(node) {
     },
-    *CallExpression(node) {
-        yield node.callee
-        for (let arg of node.arguments) {
-            yield arg.value
-        }
+    Parameter(node) {
     },
-}
-
-function getPredecessorExpressions(e: Expression) {
+    VariableDeclaration(node) {
+    },
+    FunctionExpression(node) {
+    },
+    Reference(node, scopes) {
+    },
+    MemberExpression(node) {
+    },
+    ArrayExpression(node) {
+    },
+    CallExpression(node) {
+    },
+    TemplateReference(node) {
+    },
+    ConstrainedType(node) {
+    },
+    UnaryExpression(node) {
+    }
 }
 
 export default function inferTypes(root: Analysis) {
     let scopes = createScopeMaps(root)
-    let types = new Map<ast.Typed,Expression>()
+    let resolved = new Map<ast.Typed,ast.Node>()
+    let sorted = getSortedTypedNodes(root, scopes)
+    for (let node of sorted) {
+        // first try to simplify
+        let result = simplify(node, resolved, scopes) ?? node
+        resolved.set(node, result)
+        // then try to infer types
+        let func = inferTypeFunctions[result.constructor.name]
+        result = func?.(result, resolved, scopes) ?? result
+        resolved.set(node, result)
+    }
 
     return traverse(root, {
-        leave(node) {
-            if (Literal.is(node)) {
-                return node.patch({ type: new LiteralType({ location: node.location, literal: node }) })
+        merge(node, changes, helper) {
+            let result = resolved.get(node)
+            if (result) {
+                return helper.patch(result, changes)
             }
         }
     })
---
+++ src/phases/simplify.ts
@@ -0,0 +1,61 @@
+import Assembly from "../ast/Assembly";
+import { traverse, skip } from "../Traversal";
+import Analysis from "../ast/Analysis";
+import Expression from "../ast/Expression";
+import Literal from "../ast/Literal";
+import LiteralType from "../ast/LiteralType";
+import * as ast from "../ast";
+import createScopeMaps, { ScopeMap, ScopeMaps } from "../createScopeMaps";
+import getSortedTypedNodes from "./getSortedTypedNodes";
+
+const ops = {
+    "+": (a, b) => a + b,
+    "-": (a, b) => a - b,
+    "*": (a, b) => a * b,
+    "/": (a, b) => a / b,
+}
+
+// that is some typescript kung fu right there.
+export const simplifyFunctions: { [P in keyof typeof ast]?: (e: InstanceType<typeof ast[P]>, resolved: { get<T>(t: T): T }, scope: ScopeMaps) => any} = {
+    BinaryExpression(node) {
+        if (Literal.is(node.left) && Literal.is(node.right)) {
+            let value = ops[node.operator](node.left.value, node.right.value)
+            return new Literal({ location: node.location, value })
+        }
+    },
+    UnionType(node) {
+    },
+    IntersectionType(node) {
+    },
+    Literal(node) {
+    },
+    LiteralType(node) {
+    },
+    ClassDeclaration(node) {
+    },
+    Parameter(node) {
+    },
+    VariableDeclaration(node) {
+    },
+    FunctionExpression(node) {
+    },
+    Reference(node) {
+    },
+    MemberExpression(node) {
+    },
+    ArrayExpression(node) {
+    },
+    CallExpression(node) {
+    },
+    TemplateReference(node) {
+    },
+    ConstrainedType(node) {
+    },
+    UnaryExpression(node) {
+    }
+}
+
+export default function simplify(node: Expression, resolved: Map<ast.Node,ast.Node>, scopes: ScopeMaps) {
+    let func = simplifyFunctions[node.constructor.name]
+    return func?.(node, resolved, scopes) ?? node
+}
